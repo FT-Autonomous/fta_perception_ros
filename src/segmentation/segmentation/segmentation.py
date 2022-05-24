cb6e9600e586b311f385a45b2cefe58964a04e2c
Naza <nozolo90@gmail.com>
2022-05-24 21:32:18 +0100

    Now does the exact same thing as the live preview, but in ROS

--- src/segmentation/segmentation/segmentation.py
+++ src/segmentation/segmentation/segmentation.py
@@ -1,20 +1,86 @@
+import torchvision.transforms.functional as F
+import torchvision.transforms as T
 import numpy as np
 import rclpy
 import cv2 as cv
 from rclpy.node import Node
-import sensor_msgs
-from perception_msgs.msg import Zed as ZedTopic
+from sensor_msgs.msg import Image
+from perception_msgs.srv import ForceSegment
+from std_msgs.msg import String
+import sys, os
+import torch
+
+def mask_to_color(prediction):
+    # This really could be an array
+    colors = np.array([
+        [0, 0, 0], # Background
+        [0, 0, 255], # Blue
+        [100, 100, 20], # Yellow
+        [100, 50, 0], # Large Orange
+        [120, 30, 0]  # Orange 
+    ], dtype=np.uint8)
+    
+    width = prediction.shape[0]
+    height = prediction.shape[1]
+    return colors[prediction.cpu().numpy()]
 
 class Segmentation(Node):
     def __init__(self):
         super().__init__("segmentation")
-        self.subscription = self.create_subscription(ZedTopic, "feed", self.callback, 10)
-
-    def callback(self, zed_stuff):
-        image = zed_stuff.color
+        self.declare_parameter("fta_path", os.path.join(os.environ['HOME'], "projects", "python", "ft_semantic_segmentation"))
+        self.declare_parameter("model", "cgnet")
+        self.declare_parameter("weights", os.path.join(os.environ['HOME'], "downloads", "cgnet.pt"))
+        sys.path.append(self.get_parameter("fta_path").get_parameter_value().string_value)
+        import fta
+        self.subscription = self.create_subscription(Image, "color", self.segment, 10)
+        self.force_segment = self.create_service(ForceSegment, "force_segment", self.force_segment);
+        self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+        self.model = fta.models.model_zoo.get_segmentation_model(
+            dataset="overfit",
+            pretrained_base=False,
+            backbone="resnet50",
+            model=self.get_parameter("model").get_parameter_value().string_value,
+            norm_layer=torch.nn.BatchNorm2d
+        ).to(self.device)
+        self.model.load_state_dict(torch.load(
+            self.get_parameter("weights").get_parameter_value().string_value,
+            map_location=self.device
+        ))
+        self.model.train()
+        
+    def segment(self, image):
+        data = np.array(image.data).reshape(image.height, image.width, 3)
+        self.predict_and_show(data)
+        cv.waitKey(1)
+        return image
+    
+    def force_segment(self, request, response):
+        response.segmentation_mask = self.segment(request.input)
+        return response
+        
+    def callback(self, image):
         data = np.array(image.data).reshape(image.height, image.width, 3);
         cv.imshow("ROS", data)
         cv.waitKey(1)
+
+    def predict_and_show(self, original_image, interval=25):
+        '''Takes a numpy HWC, BGR image, makes predictions
+        And shows them on the screen'''
+        new_height = int(512 * original_image.shape[1] / original_image.shape[0])
+        resized_image = cv.resize(original_image, (new_height - new_height % 64, 512), interpolation=cv.INTER_NEAREST)
+        image = cv.cvtColor(resized_image, cv.COLOR_BGR2RGB)
+        normal = T.Normalize([.485, .456, .406], [.229, .224, .225])
+        tensor = normal(torch.from_numpy(image.transpose(2, 0, 1)).to(self.device).float())
+        #TODO: Publish both argmax and softmax versions
+        pred = torch.nn.Softmax(0)(self.model(tensor[None, ...])[0][0]).argmax(0)
+        #pred[0, pred[0, ...] > 0.2] = 1
+        mask = cv.cvtColor(mask_to_color(pred), cv.COLOR_RGB2BGR)
+        #cv.imshow("Image only", resized_image)
+        result = np.uint8(resized_image / 2 + mask / 2.0)
+        cv.imshow("Mask and Image", result)
+        #cv.imshow("Mask only", mask)
+        #cv.imwrite(f"test/{random.randint(0, 1000000)}.png", result)
+        return pred
 
 def main(args=None):
     rclpy.init(args=args)
--- src/video_capture/src/video_capture.cpp
+++ src/video_capture/src/video_capture.cpp
@@ -1,3 +1,8 @@
+#include "rclcpp/client.hpp"
+#include "rclcpp/executors.hpp"
+#include "rclcpp/logging.hpp"
+#include <future>
+#include <ostream>
 #include <vector>
 #include <memory>
 #include <functional>
@@ -11,50 +16,81 @@
 
 #include <sensor_msgs/msg/image.hpp>
 #include <sensor_msgs/image_encodings.hpp>
-#include <perception_msgs/msg/zed.hpp>
 #include <std_msgs/msg/header.hpp>
 
 #include <opencv2/core.hpp>
 #include <opencv2/videoio.hpp>
 #include <opencv2/imgcodecs.hpp>
 #include <opencv2/highgui.hpp>
+#include <perception_msgs/srv/force_segment.hpp>
 
 using namespace std::chrono_literals;
 
 class VideoCaptureNode
     : public rclcpp::Node {
 private:
-    using ZedTopic = perception_msgs::msg::Zed;
-    
+    using Image = sensor_msgs::msg::Image;
+    using ForceSegment = perception_msgs::srv::ForceSegment;
+    using ForceSegmentClient = rclcpp::Client<ForceSegment>;
+    using ImagePublisher = rclcpp::Publisher<Image>;
+
+    unsigned int frame_id;
     cv::VideoCapture camera;
+    Image current_ros_image;
     cv::Mat image_buffer;
-    rclcpp::Publisher<ZedTopic>::SharedPtr publisher;
+    ImagePublisher::SharedPtr color_publisher;
+    ImagePublisher::SharedPtr depth_publisher;
+    ForceSegmentClient::SharedPtr force_segment_client;
     rclcpp::TimerBase::SharedPtr shot_clock;
+    rclcpp::TimerBase::SharedPtr force_segment_future_timer;
+    std::shared_future<ForceSegmentClient::SharedResponse> force_segment_future;
+    bool waiting;
 
 private:
     void capture_image() {
+	
 	camera.read(this->image_buffer);
-	ZedTopic zed_topic;
-	sensor_msgs::msg::Image im;
-	im.header.stamp = rclcpp::Time();
-	im.width = image_buffer.cols;
-	im.height = image_buffer.rows;
-	im.encoding = sensor_msgs::image_encodings::BGR8;
-	im.is_bigendian = false;
-	im.step = image_buffer.cols * 3;
-	im.data = image_buffer.reshape(1, image_buffer.rows * image_buffer.cols * image_buffer.channels());
-	zed_topic.color = im;
-	this->publisher->publish(zed_topic);
+
+	current_ros_image.header.frame_id = std::to_string(this->frame_id++);
+	current_ros_image.width = this->image_buffer.cols;
+	current_ros_image.height = this->image_buffer.rows;
+	current_ros_image.encoding = sensor_msgs::image_encodings::BGR8;
+	current_ros_image.step = this->image_buffer.cols * 3;
+	current_ros_image.data = this->image_buffer.reshape(1, image_buffer.rows * image_buffer.cols * image_buffer.channels());
+	
+	auto request = std::make_shared<ForceSegment::Request>();
+	request->input = current_ros_image;
+	waiting = true;
+	this->force_segment_future = this->force_segment_client->async_send_request(request);
+	this->force_segment_future_timer = this->create_wall_timer(5ms, std::bind(&VideoCaptureNode::try_finish, this));
+
+    }
+
+    void try_finish() {
+	if (this->force_segment_future.wait_for(0s) == std::future_status::ready) {
+	    this->force_segment_future_timer->cancel();
+	    this->current_ros_image.data = this->image_buffer.reshape(1, image_buffer.rows * image_buffer.cols * image_buffer.channels());
+	    this->color_publisher->publish(this->current_ros_image);
+	    this->depth_publisher->publish(this->force_segment_future.get()->segmentation_mask);
+	    waiting = false;
+	}
     }
 public:
     VideoCaptureNode(const rclcpp::NodeOptions & options = rclcpp::NodeOptions())
-	: rclcpp::Node("video_capture", options)
+	: rclcpp::Node("video_capture", options),
+	  frame_id(0),
+	  waiting(false)
     {
 	using namespace std::placeholders;
 	this->declare_parameter<int>("camera", 1);
 	this->camera = cv::VideoCapture(this->get_parameter("camera").get_parameter_value().get<int>());
-	this->publisher = this->create_publisher<ZedTopic>("feed", 1);
+	this->color_publisher = this->create_publisher<Image>("color", 1);
+	this->depth_publisher = this->create_publisher<Image>("depth", 1);
 	this->shot_clock = this->create_wall_timer(0.5s, std::bind(&VideoCaptureNode::capture_image, this));
+	this->force_segment_client = this->create_client<ForceSegment>("force_segment");
+	while (not this->force_segment_client->wait_for_service(1s)) {
+	    RCLCPP_INFO(this->get_logger(), "Force Segment Server not available! Waiting...");
+	}
     }
 };
 
