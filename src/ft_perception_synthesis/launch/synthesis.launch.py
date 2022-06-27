8885dd47a3cef46aef42b9cb1659bd6e52fc1db4
Chinaza Uzoukwu <uzoukwuc@tcd.ie>
2022-06-28 00:20:05 +0100

    Input source now defaults to the bag

--- src/ft_perception_synthesis/launch/synthesis.launch.py
+++ src/ft_perception_synthesis/launch/synthesis.launch.py
@@ -1,6 +1,6 @@
 from launch import LaunchDescription
 from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
-from launch.actions import GroupAction, DeclareLaunchArgument
+from launch.actions import GroupAction, DeclareLaunchArgument, ExecuteProcess
 from launch_ros.actions import Node, PushRosNamespace
 from launch_ros.substitutions import FindPackageShare
 from launch.conditions import IfCondition
@@ -8,13 +8,20 @@ from launch.substitutions import PythonExpression
 
 def generate_launch_description():
     return LaunchDescription([
+        DeclareLaunchArgument("source", default_value="bag"),
+        DeclareLaunchArgument("bag", default_value="static"),
         GroupAction(
             actions=[
                 PushRosNamespace('perception'),
                 Node(package='zed_capture',
                      executable='zed_capture',
                      name='video_capture',
-                     parameters=[PathJoinSubstitution([FindPackageShare('ft_perception_synthesis'), 'config', 'video_capture.yaml'])]),
+                     parameters=[PathJoinSubstitution([FindPackageShare('ft_perception_synthesis'), 'config', 'video_capture.yaml'])],
+                     condition=IfCondition(PythonExpression(["'", LaunchConfiguration("source"), "'== 'zed'"]))),
+                ExecuteProcess(
+                    cmd=['ros2 bag play --loop ', PathJoinSubstitution([FindPackageShare('ft_perception_synthesis'), 'bags', LaunchConfiguration('bag')])],
+                    shell=True, 
+                    condition=IfCondition(PythonExpression(["'", LaunchConfiguration("source"), "'== 'bag'"]))),
                 Node(package='segmentation',
                      executable='segmentation',
                      name='segmentation',
