4211adda1b9500ed7474c0bbb96a36efa7e66e67
Chinaza Uzoukwu <uzoukwuc@tcd.ie>
2022-06-27 22:29:57 +0100

    Fixed bugs. Downsampling is now performed before clustering.

--- src/ft_perception_synthesis/src/perception.cpp
+++ src/ft_perception_synthesis/src/perception.cpp
@@ -49,12 +49,12 @@ public:
 	: rclcpp::Node("perception_node") {
 	using namespace std::placeholders;
 	this->declare_parameter<int>("service_wait_time_ms", 1000);
+	this->declare_parameter<int>("downsample_factor", 5);
 	this->cluster_client = this->create_client<Cluster>("cluster");
 	this->segment_client = this->create_client<ForceSegment>("force_segment");
 	this->center_estimation_client = this->create_client<GetCenters>("estimate_centers");
 	this->cone_publisher = this->create_publisher<ConeArray>("cones", 1);
 	this->zed_subscription = this->create_subscription<Zed>("zed", 1, std::bind(&PerceptionNode::segment, this, _1));
-        RCLCPP_INFO(this->geet_logger(), "Lol -11");
     }
 
     template <typename T>
@@ -68,55 +68,47 @@ public:
 
     void segment(Zed::SharedPtr zed_msg) {
         if (lock.try_lock()) {
-        RCLCPP_INFO(this->get_logger(), "Lol -10");
             auto segment_request = std::make_shared<ForceSegment::Request>();
             this->last_depth_map = zed_msg->depth;
             segment_request->input = zed_msg->color;
             this->make_sure_service_is_ready<ForceSegment>(this->segment_client);
             this->segment_client->async_send_request(segment_request, std::bind(&PerceptionNode::cluster, this, _1));
-        RCLCPP_INFO(this->get_logger(), "Lol -9");
         }
     }
 
     void cluster(std::shared_future<ForceSegment::Response::SharedPtr> future) {
 
-        int factor = 10;
+	int factor = this->get_parameter("downsample_factor").get_parameter_value().get<int>();
 
-        RCLCPP_INFO(this->geet_logger(), "Lol 0");
-
-        Image & image = last_depth_map;
-        Image new_image;
-        new_image.width = image.width / factor;
-        new_image.height = image.height / factor;
-        std::vector<float> floats(image.width * image.height * 3);
-        memcpy(floats.data(), image.data.data(), floats.size() * 4);
-
-        RCLCPP_INFO(this->geet_logger(), "Lol 1");
-
-        cv::Mat cv_floats(floats);
-        cv::Mat out_seg;
-        cv::resize(cv_floats, out_seg, cv::Size(), new_image.width, new_image.height, cv::INTER_NEAREST);
-        new_image.data.resize(new_image.width * new_image.height * 3);
-        memcpy(new_image.data.data(), out_seg.data, 4 * new_image.width  * new_image.height  * 3);
-
-        RCLCPP_INFO(this->geet_logger(), "Lol 2");
+        Image & depth = last_depth_map;
+        Image new_depth;
+        new_depth.width = depth.width / factor;
+        new_depth.height = depth.height / factor;
+	
+        cv::Mat floats = cv::Mat(depth.height, depth.width, CV_32FC3);
+	memcpy(floats.data, depth.data.data(), depth.data.size());
+	
+        cv::Mat out_depth;
+        cv::resize(floats, out_depth, cv::Size(new_depth.width, new_depth.height), cv::INTER_NEAREST);
+	
+        new_depth.data.resize(4 * new_depth.width * new_depth.height * 3);
+        memcpy(new_depth.data.data(), out_depth.data, new_depth.data.size());
 
         Image & seg = future.get()->segmentation_mask;
-        Image new_depth;
-        new_depth.width = seg.width / factor;
-        new_depth.height = seg.height / factor;
-        std::vector<uchar> chars(seg.width * seg.height * 3);
-        memcpy(chars.data(), seg.data.data(), chars.size());
-        cv::Mat cv_chars(chars);
-        cv::Mat out_depth;
-        cv::resize(cv_chars, out_depth, cv::Size(), new_depth.width, new_depth.height, cv::INTER_NEAREST);
-        new_depth.data.resize(new_depth.width * new_depth.height * 3);
-        memcpy(new_depth.data.data(), out_depth.data, new_depth.width  * new_depth.height  * 3);
+        Image new_segmentation_mask;
+        new_segmentation_mask.width = seg.width / factor;
+        new_segmentation_mask.height = seg.height / factor;
+        cv::Mat chars = cv::Mat(seg.height, seg.width, CV_8UC1);
+	memcpy(chars.data, seg.data.data(), seg.data.size());
+	cv::Mat downsampled_segmentation_mask;
+        cv::resize(chars, downsampled_segmentation_mask, cv::Size(new_segmentation_mask.width, new_segmentation_mask.height), cv::INTER_NEAREST);
+        new_segmentation_mask.data.resize(new_segmentation_mask.width * new_segmentation_mask.height);
+        memcpy(new_segmentation_mask.data.data(), downsampled_segmentation_mask.data, new_segmentation_mask.data.size());
 
 	auto cluster_request = std::make_shared<Cluster::Request>();
-	this->last_segmentation_mask = new_image;
-	cluster_request->segmentation_mask = new_image;
-    this->last_depth_map = new_depth;
+	this->last_segmentation_mask = new_segmentation_mask;
+	cluster_request->segmentation_mask = new_segmentation_mask;
+	this->last_depth_map = new_depth;
 	cluster_request->points = new_depth;
 	this->make_sure_service_is_ready<Cluster>(this->cluster_client);
 	this->cluster_client->async_send_request(cluster_request, std::bind(&PerceptionNode::estimate_centers, this, _1));
